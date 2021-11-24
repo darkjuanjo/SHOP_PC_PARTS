5ee04097edb6f22904d812de6b2b994d704360d3
hanielchang <haniellloyd@gmail.com>
2021-11-24 00:38:29 -0800

    combining with backend

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,25 +1,29 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 function Header(props) {
-    const {countCartItems} = props;
+    const { countCartItems } = props;
     return (
         <header className="row block center">
             <div>
-                <a href="#/">
-                    <h1>Shop PC Parts</h1>
-                </a>
+                <Link to="/">
+                    <a href="#/">
+                        <h1>Shop PC Parts</h1>
+                    </a>
+                </Link>
             </div>
             <div>
                 <a href="#/cart">
-                    
-                    Cart { ' '}
-                    {countCartItems? (
+
+                    Cart {' '}
+                    {countCartItems ? (
                         <button className="badge">{countCartItems}</button>
                     ) : (
                         ''
                     )};
-                    
-                    </a> {' '} 
-                     <a href="#/SignIn">SignIn</a>
+
+                </a> {' '}
+                <Link to="/LoginForm">Sign In</Link>
             </div>
         </header>
     );
