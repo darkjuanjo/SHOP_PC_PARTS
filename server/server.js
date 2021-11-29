3f8380c29387ca20d634f439532d1a8b78367fc2
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-11-28 23:51:34 -0800

    git testing for heroku issues

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 require("dotenv").config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const app = express();
